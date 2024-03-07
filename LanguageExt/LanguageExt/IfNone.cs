564ca781627690afa00fc744a2188cc8c47a548b
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-03-08 07:56:52 +0900

    feat : add IfNone

---
+++ LanguageExt/LanguageExt/IfNone.cs
@@ -0,0 +1,16 @@
+﻿namespace LanguageExt;
+
+public class IfNone
+{
+    public int GetValue(int? value)
+    {
+        return value.IfNone(0);
+    }
+
+    [Fact]
+    public void IfNoneTest()
+    {
+        int? number = null;
+        number.IfNone(0).Should().Be(0);
+    }
+}
