093cc7eee36081cc0278802f976ff409a69fd763
박주형 <juhyung.park@mirero.co.kr>
2024-03-07 17:28:43 +0900

    feat : add TryParse

--- LanguageExt/LanguageExt/TryParse.cs
+++ LanguageExt/LanguageExt/TryParse.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LanguageExt
+﻿namespace LanguageExt_Test;
+public class TryParse
 {
-    internal class TryParse
+    [Fact]
+    public void TryParseTest()
     {
+        int normalValue = parseInt("123").IfNone(0);
+        normalValue.Should().Be(123);
+
+        int nullValue = ifNone(parseInt(null), 0);
+        nullValue.Should().Be(0);
+        
+        parseInt("123").Match(
+          Some: x => x * 2,
+          None: () => 0
+        ).Should().Be(246);
+        
+        match(parseInt("123"),
+            Some: x => x * 2,
+            None: () => 0
+        ).Should().Be(246);
     }
 }
