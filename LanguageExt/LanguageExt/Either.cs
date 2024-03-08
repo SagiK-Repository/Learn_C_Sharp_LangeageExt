ad6d718d1e97defba979659c87795915fa4ffe38
박주형 <juhyung.park@mirero.co.kr>
2024-03-08 12:29:36 +0900

    feat : Add Either

--- LanguageExt/LanguageExt/Either.cs
+++ LanguageExt/LanguageExt/Either.cs
@@ -2,12 +2,12 @@
 
 public class Either
 {
-    public static Either<string, int> ValidateNumber(string input)
+    public Either<string, int> ValidateNumber(string input)
     {
         if (int.TryParse(input, out int number))
             return number;
         else
-            return $"Invalid number";
+            return "Invalid number";
     }
 
     [Fact]
@@ -27,4 +27,63 @@ public class Either
         // Assert
         result.IsRight.Should().BeTrue();
     }
+    public Either<Exception, string> GetHtml(string url)
+    {
+        var httpClient = new HttpClient(new HttpClientHandler());
+        try
+        {
+            var httpResponseMessage = httpClient.GetAsync(url).Result;
+            return httpResponseMessage.Content.ReadAsStringAsync().Result;
+        }
+        catch (Exception ex) { return ex; }
+    }
+
+    [Fact]
+    public void either_example()
+    {
+
+        var leftResult = GetHtml("unknown url"); 
+        leftResult.Match(
+            Left: ex => ex.Should().BeOfType<InvalidOperationException>(),
+            Right: r => Assert.True(true)
+        );
+
+        var result = GetHtml("https://www.google.com");
+
+        result.Match(
+                Left: ex => ex.Should().BeOfType<InvalidOperationException>(),
+                Right: r => Assert.True(true)
+            );
+    }
+
+    //public async Task<Either<string, int>> ValidateNumberAsync(string input)
+    //{
+    //    await Task.Delay(100); // 비동기 작업 시뮬레이션
+
+    //    if (int.TryParse(input, out int number))
+    //        return number;
+    //    else
+    //        return "Invalid number";
+    //}
+
+    //[Fact]
+    //public async Task ValidateNumberAsync_ValidInput_ReturnsRightAsync()
+    //{
+    //    // Arrange
+    //    string input = "123";
+
+    //    // Act
+    //    EitherAsync<string, int> result = new EitherAsync<string, int>(await ValidateNumberAsync(input));
+
+    //    // Assert
+    //    await result.MatchAsync(
+    //        Right: r => r.Should().Be(123),
+    //        LeftAsync: async s => 
+    //        {
+    //                await Task.Delay(100);
+    //                s.Should().Be("Invalid number");
+    //        });
+
+    //    result.IsRight.Should().Be(true);
+    //}
 }
