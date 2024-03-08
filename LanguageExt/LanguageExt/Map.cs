d09e663dec413b348ecacd85f848c99c1efa778d
박주형 <juhyung.park@mirero.co.kr>
2024-03-08 10:14:42 +0900

    feat : add Map

--- LanguageExt/LanguageExt/Map.cs
+++ LanguageExt/LanguageExt/Map.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LanguageExt_Test;
 
-namespace LanguageExt
+public class Map
 {
-    internal class Map
+    public int Squar(int x) => x * x;
+
+    [Fact]
+    public void MapTest()
     {
+        Option<int> value = 10;
+        value.Map(x => x * 1.5f).IfNone(0.0f).Should().Be(15f);
+
+        var values = new List<int>() { 1, 2, 3 };
+        var resultList = values.Map(x => x * 2);
+        resultList.Should().Equal(new List<int>() { 2, 4, 6 });
+
+
+        var languageExtList = List(1, 2, 3);
+        languageExtList.Map(Squar);
+        resultList.Should().Equal(List(2, 4, 6));
     }
 }
