0d04f3fbe6015c9898f125615cd197f87bf4bd65
박주형 <juhyung.park@mirero.co.kr>
2024-03-08 11:08:33 +0900

    feat : add mem

---
+++ LanguageExt/LanguageExt/Mem.cs
@@ -0,0 +1,54 @@
+﻿using LanguageExt.Common;
+
+namespace LanguageExt_Test;
+
+public class Mem
+{
+    static Func<string, string> GenerateGuidForUser = user => user + ":" + Guid.NewGuid();
+    static Func<string, string> GenerateGuidForUserMemoized = memo(GenerateGuidForUser);
+
+    static public string GenerateGuidForUserMethod(string user) => user + ":" + Guid.NewGuid();
+    public Func<string, string> GenerateGuidForUserMemoizedMethod = memo<string, string>(GenerateGuidForUserMethod);
+
+
+    [Fact]
+    public void Memorization_Test()
+    {
+        string result = GenerateGuidForUserMemoized("spongebob");
+        GenerateGuidForUserMemoized("spongebob").Should().Be(result);
+
+        string resultString = GenerateGuidForUserMemoizedMethod("spongebob");
+        GenerateGuidForUserMemoizedMethod("spongebob").Should().Be(resultString);
+
+        string notMatchString = GenerateGuidForUser("Hello");
+        GenerateGuidForUser("Hello").Should().NotBe(notMatchString);
+    }
+
+    [Fact]
+    public void Memorization_LiveTest()
+    {
+        string result = string.Empty;
+        {
+            Func<string, string> MemoizedMethod = memo(GenerateGuidForUser);
+            result = MemoizedMethod("spongebob");
+        }
+
+        Func<string, string> AfterMemoizedMethod = memo(GenerateGuidForUser);
+        AfterMemoizedMethod("spongebob").Should().NotBe(result);
+    }
+
+    static Func<string, Task<string>> GenerateGuidForUserAsync = async user =>
+    {
+        await Task.Delay(100);  // 비동기 작업을 시뮬레이션합니다.
+        return user + ":" + Guid.NewGuid();
+    };
+
+    [Fact]
+    public async Task Memorization_AsyncTest()
+    {
+        Func<string, Task<string>> MemoizedMethod = memo(GenerateGuidForUserAsync);
+        string result = await MemoizedMethod("spongebob");
+        string afterResult = await MemoizedMethod("spongebob");
+        result.Should().Be(afterResult);
+    }
+}
